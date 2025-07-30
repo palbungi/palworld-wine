6b6d9f70228303ec2b741b2b6d0f5ae58f97dc5b
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-07-31 06:01:23 +0900

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -8,7 +8,7 @@ SCRIPT_PATH="$SCRIPT_DIR/backup.sh"
 chmod +x "$SCRIPT_PATH"
 
 # 크론탭 명령어 구성
-CRON_JOB="30 * * * * /bin/bash $SCRIPT_PATH
+CRON_JOB="30 * * * * /bin/bash $SCRIPT_PATH"
 
 # 기존 크론탭에서 중복 제거 후 새 항목 추가
 ( sudo crontab -l 2>/dev/null | grep -v "$SCRIPT_PATH" ; echo "$CRON_JOB" ) | sudo crontab -
