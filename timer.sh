e35595dc68d250b3675d43eb1e152ebea65d2237
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-07-31 13:28:15 +0900

    Update timer.sh

--- timer.sh
+++ timer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG_FILE="/home/YOUR_USERNAME/default.env"
-SCRIPT_FILE="/home/YOUR_USERNAME/regular_maintenance.sh"
+CONFIG_FILE="/home/YOUR_USERNAME/palworld-wine/default.env"
+SCRIPT_FILE="/home/YOUR_USERNAME/palworld-wine/regular_maintenance.sh"
 
 # 1. Check ADMIN_PASSWORD
 ADMIN_PASSWORD=$(grep "^ADMIN_PASSWORD=" "$CONFIG_FILE" | cut -d= -f2)
