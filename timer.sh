3b5e7be68386b6d03b4e509e7a87799c397c14e8
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-08-01 01:43:31 +0900

    Update timer.sh

--- timer.sh
+++ timer.sh
@@ -7,6 +7,7 @@ SCRIPT_FILE="/home/YOUR_USERNAME/palworld-wine/regular_maintenance.sh"
 ADMIN_PASSWORD=$(grep "^ADMIN_PASSWORD=" "$CONFIG_FILE" | cut -d= -f2)
 
 if [ "$ADMIN_PASSWORD" == "adminpasswd" ]; then
+    clear
     read -s -p "운영자 비밀번호를 입력하세요: " NEW_PASSWORD
     echo
     sed -i "s/^ADMIN_PASSWORD=.*/ADMIN_PASSWORD=$NEW_PASSWORD/" "$CONFIG_FILE"
@@ -14,12 +15,16 @@ fi
 
 # 2. Clear existing crontab
 crontab -r
+clear
 echo "기존 팰월드서버 재시작 목록을 삭제했습니다."
-
+echo
 # 3. Menu
 echo "0. 팰월드서버 재시작 안함"
+echo
 echo "1. 하루 횟수만 지정 (자동 시간 계산)"
+echo
 echo "2. 하루 횟수와 시간 지정"
+echo
 read -p "번호를 선택하세요: " MODE
 
 # 6. Crontab header
@@ -30,8 +35,10 @@ if [ "$MODE" == "0" ]; then
     echo "팰월드서버 재시작을 하지 않도록 설정했습니다. 스크립트를 종료합니다."
     exit 0
 elif [ "$MODE" == "1" ]; then
+    echo
     read -p "하루에 몇 번 실행할까요? (0 입력시 종료): " COUNT
     if [ "$COUNT" -eq 0 ]; then
+        echo
         echo "0번 입력으로 종료합니다."
         exit 0
     fi
@@ -41,12 +48,15 @@ elif [ "$MODE" == "1" ]; then
         CRON_ENTRIES+=("0 $HOUR * * * $SCRIPT_FILE")
     done
 elif [ "$MODE" == "2" ]; then
+    echo
     read -p "하루에 몇 번 실행할까요? (0 입력시 종료): " COUNT
     if [ "$COUNT" -eq 0 ]; then
+        echo
         echo "0번 입력으로 종료합니다."
         exit 0
     fi
     for ((i=1; i<=COUNT; i++)); do
+        echo
         read -p "$i 번째 실행 시간을 입력하세요 (0~24): " HOUR
         [ "$HOUR" == "24" ] && HOUR="0"
         CRON_ENTRIES+=("0 $HOUR * * * $SCRIPT_FILE")
@@ -58,5 +68,5 @@ fi
 
 # Apply crontab
 ( for ENTRY in "${CRON_ENTRIES[@]}"; do echo "$ENTRY"; done ) | crontab -
-
+echo
 echo "팰월드서버 재시작이 성공적으로 설정되었습니다."
