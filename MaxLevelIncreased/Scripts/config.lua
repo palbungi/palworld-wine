56df7fab22434bb37377cc98635660d162907a2a
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-08-01 23:33:58 +0900

    Add files via upload

---
+++ MaxLevelIncreased/Scripts/config.lua
@@ -0,0 +1,23 @@
+-- made by caites
+local palConfig = {
+
+    serverAcknowledgedPossession = false,
+
+    -- Change those for new mod or different version
+    palModName = "Max Level Increased",
+    palModVersion = "1.15",
+    palGameVersion = "0.4.11.0",	
+
+    -- Those are generic for every mod
+    palCheckRestart = 0,
+    palRedColorWrapperFront = "\27[31m",
+    palRedColorWrapperBack = "\27[0m\n",
+
+    -- Character Max Level
+	CharacterMaxLevel = 99,	
+
+	 -- Guild Character Max Level
+	GuildCharacterMaxLevel = 99
+
+}
+return palConfig
